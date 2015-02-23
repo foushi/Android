a1be9c47047deb5632b21936fad5822889d81076
Fabrici Jean-Christophe <jcfabrici@gmail.com>
2015-02-23 20:11:10 +0100

    projet

---
+++ TheNewBoston/src/com/example/thenewboston/Menu.java
@@ -0,0 +1,43 @@
+package com.example.thenewboston;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Menu extends ListActivity{
+	
+	String classes[]= {"Starting_point","TextPlay","Email" 
+			,"example3","example4","example5","example6"} ;
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter <String>(Menu.this, android.R.layout.simple_list_item_1, classes));
+	}
+
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		String cheese = classes[position];
+		Class ourClass;
+		try {
+			ourClass = Class.forName("com.example.thenewboston."+cheese);
+			Intent ourIntent = new Intent (Menu.this, ourClass);
+			startActivity(ourIntent);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+	
+	
+}
